78bbcf9cc7b9e60f88db62369a6b48de142b413b
Gabox13 <gabops0511@gmail.com>
2024-05-04 14:19:42 -0600

    cambio puerto y host

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from src.routes import Profesores
 
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def hello_world():
     return 'Hello world!'
