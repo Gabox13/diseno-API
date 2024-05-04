4abf665d5048ae653e3d2a5fc1ce311e01e3cac7
Gabox13 <gabops0511@gmail.com>
2024-05-04 01:22:08 -0600

    print hello

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ if __name__ == '__main__':
     app.register_blueprint(Profesores.main, url_prefix='/profes')
 
     app.register_error_handler(404, page_not_found)
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8000,debug=True)
